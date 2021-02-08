38c02ea5670e6ec563ff311637bf7b227673b0d5
AnnJay <orange_world@inbox.ru>
2021-02-08 21:44:26 +0300

    added blog, video and design sections

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,16 @@ $(function(){
     autoplaySpeed: 4000,
   });
 
+  $('.partners__list').slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
+  });
+
   var mixer1 = mixitup('.popular__products', {
     selectors: {
       control: '.filter-1'
